150ac81a23c3255e9f2f31742719b82421c75ab1
Nick D'Esposito <Nicholas D'Esposito>
2022-05-27 21:35:23 -0400

    Need to fix fetch error on books list tab. May require some time consuming troubleshooting

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,48 @@
-
 // Your Code Here
+
+// async function retrieveBooks() {
+//     {
+//         let bookslist = await fetch("http://localhost:3001/listBooks", {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "Application/json",
+//             },
+//             body: {
+//                 id: i,
+//                 title: bookslist[i].title,
+//             },
+//         });
+//     }
+//    let bookDiv = document.createElement("div");
+
+//     let response = await booksList.json();
+// }
+let root = document.getElementById("root");
+root.style.height = "3vh";
+root.style.width = "0vh";
+
+function inputNew(name, text) {
+    name = document.createElement("input");
+    let textOfInput = document.createElement("p");
+
+    textOfInput.style.position = "relative";
+    textOfInput.style.left = "3vh";
+    textOfInput.innerHTML = text;
+    name.style.position = "relative";
+    name.style.left = "1vw";
+    let stylebtn = document.createElement("button");
+    stylebtn.innerHTML = "Save";
+    stylebtn.style.position = "relative";
+    stylebtn.style.left = "50vh";
+    stylebtn.style.bottom = "7vh";
+    name.style.display = "block";
+    stylebtn.style.display = "inline";
+    document.body.append(textOfInput);
+    document.body.append(name);
+    document.body.append(stylebtn);
+}
+
+inputNew("inputShinobi", "Shinobi Initiative");
+inputNew("inputTess", "Tess Wonder Dog");
+inputNew("inputAnnals", "The annals of Atathrae");
+inputNew("inputWarp", "Warp");
